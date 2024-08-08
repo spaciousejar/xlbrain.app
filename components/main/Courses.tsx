035d92d86266ba2b880afe72c71db0e0767c50d2
Vicky Jadhav <76901120+spaciousejar@users.noreply.github.com>
2024-08-08 17:07:15 +0530

    Update Courses.tsx

--- components/main/Courses.tsx
+++ components/main/Courses.tsx
@@ -12,24 +12,24 @@ const Courses = () => {
         </h1>
       </div>
     <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10 items-center justify-center">
-      <p className="text-lg text-white my-5 max-w-[600px] flex-wrap">
-        <h2 className="text-[25px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20 text-center">
-        Course Highlights
-        </h2>
-        Harness the power of visualization to improve memory retention and
-        comprehension.
-        Learn how to create mental images to represent complex information.
-        Discover effective learning techniques like the Casting Director Method.
-        Engage with real-life examples and stories to see how these methods in
-        practice.
-        Join XL BRAIN and take your learning to the next level. Let's make learning a
-        visual, engaging, and impactful experience!
-      </p>
-        <Videoplayer 
-        src="https://www.youtube.com/embed/hHuG7FIKgtc?si=0TQ6reJOghpJjPpP" 
-        title=""
-        description=""/>
-    </div></>
+     <p className="text-lg text-white my-5 max-w-[600px] flex-wrap">
+  <h2 className="text-[25px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20 text-center">
+  Course Highlights
+  </h2>
+  Harness the power of visualization to improve memory retention and
+  comprehension.
+  Learn how to create mental images to represent complex information.
+  Discover effective learning techniques like the Casting Director Method.
+  Engage with real-life examples and stories to see how these methods in
+  practice.
+  Join XL BRAIN and take your learning to the next level. Let&apos;s make learning a
+  visual, engaging, and impactful experience!
+    </p>
+  <Videoplayer 
+  src="https://www.youtube.com/embed/hHuG7FIKgtc?si=0TQ6reJOghpJjPpP" 
+  title=""
+  description=""/>
+</div></>
     
   );
 };
